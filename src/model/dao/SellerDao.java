ec7558fbd08f4bfcc75df421e339c65080e7845b
Valdemar Carvalho <valdemar.victor.leite.carvalho@ccc.ufcg.edu.br>
2024-01-04 19:10:13 -0300

    findById implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -6,8 +6,8 @@ import model.entities.Seller;
 
 public interface SellerDao {
 	
-	void insert(Seller seller);
-	void update(Seller seller);
+	void insert(Seller obj);
+	void update(Seller obj);
 	void deleteById(Integer id);
 	Seller findById(Integer id);
 	List<Seller> findAll();
